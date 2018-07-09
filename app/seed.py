910923602dced0086b3c3555a1928752c8465134
AndresHernandezT <andres.hernandezt@mail.udp.cl>
2018-07-08 21:48:29 -0400

    Update seed.py

--- app/seed.py
+++ app/seed.py
@@ -3,7 +3,7 @@ import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
 cur = conn.cursor()
 sql ="""
-insert into animes (id,nombre,cant_capitulos,tipo,autor_id,estudio_id) values 
+insert into animes (nombre,cant_capitulos,tipo,autor_id,estudio_id) values 
 ('Death Note','38','Anime','1','1'), 
 ('Watashi ga Motete Dousunda'.'12','anime','2','6'), 
 ('Orange','13','anime','3','7'), 
