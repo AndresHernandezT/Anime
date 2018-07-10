482cfda7bdc872fc8b242f48f80b2ab85050216a
AndresHernandezT <andres.hernandezt@mail.udp.cl>
2018-07-10 06:00:46 -0400

    Update views.py

--- app/views.py
+++ app/views.py
@@ -9,26 +9,77 @@ cur = conn.cursor()
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	return render_template('index.html',generos=generos)
 
 @app.route('/animes')
 def animes():
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
 	sql = "select * from Animes"
 	cur.execute(sql)
-	resultados = cur.fetchall()
-	print resultados
-	final = ""
-	for i in resultados:
-		final += str(i[1]) + "<br/>"
-	return final
+	animes = cur.fetchall()
+	return render_template('index.html',animes=animes,generos=generos)
 
-@app.route('/adios')
-def adios():
-        return "No me quiero venir, senores homeros"
+@app.route('/categorias')
+def categorias():
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	return render_template('categorias.html',generos=generos)
+
+@app.route('/personajes')
+def personajes():
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	sql = "select * from Personajes"
+	cur.execute(sql)
+	personajes = cur.fetchall()
+	return render_template('personajes.html',generos=generos,personajes=personajes)
+
+@app.route('/autores')
+def autores():
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	sql = "select * from Autores"
+	cur.execute(sql)
+	autores = cur.fetchall()
+	return render_template('autores.html',generos=generos,autores=autores)
+
+@app.route('/estudios')
+def estudios():
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	sql = "select * from Estudios"
+	cur.execute(sql)
+	estudios = cur.fetchall()
+	return render_template('estudios.html',generos=generos,estudios=estudios)
+
+@app.route('/categorias/<int:id>')
+def categorias_id(id):
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	sql = "select nombre from Generos where id = " + str(id)
+	cur.execute(sql)
+	resultado = cur.fetchall()
+	sql = "select * from Animes, Generos, Animes_Generos where anime_id = Animes.id and genero_id = Generos.id and Generos.id = " + str(id)
+	cur.execute(sql)
+	animes = cur.fetchall()
+	return render_template('categorias_id.html',animes=animes,generos=generos)
 
 @app.route('/animes/<int:id>')
 def animes_id(id):
-	sql = "select nombre from Animes where id = " + str(id)
+	sql = "select * from Animes where id = " + str(id)
 	cur.execute(sql)
-	resultados = cur.fetchall()
-	return str(resultados[0][0])
+	resultado = cur.fetchall()
+	sql = "select * from Generos"
+	cur.execute(sql)
+	generos = cur.fetchall()
+	return render_template('animes_id.html',generos=generos,resultado=resultado)
