9663571cd83b5d8f4c9eaf11e3563b671b9e1652
AndresHernandezT <andres.hernandezt@mail.udp.cl>
2018-07-09 23:36:43 -0400

    Update setup.py

--- app/setup.py
+++ app/setup.py
@@ -1,53 +1,5 @@
 from configuraciones import *
 import psycopg2
-conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
-
-
-cur = conn.cursor()
-sql ="""DROP SCHEMA public CASCADE;
-CREATE SCHEMA public;"""
-
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE Animes
-           (id serial PRIMARY KEY, nombre varchar(40), cant_capitulos integer, tipo varchar(40), autor_id integer, estudio_id integer);
-"""
-
-cur.execute(sql)
-
-
-sql ="""
-CREATE TABLE Estados
-           (anime_id integer PRIMARY KEY, tipo_de_estado varchar(40), fecha_emision varchar(10), fecha_termino varchar(10));
-"""
-
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE Animes_generos
-           (anime_id integer, genero_id integer);
-"""
-
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE  Autores
-           (id serial PRIMARY KEY, nombre varchar(140));
-"""
-
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE Estudios
-           (id serial PRIMARY KEY, nombre varchar(140));
-"""
-
-cur.execute(sql)
-
-sql = """
-CREATE TABLE Personajesfrom configuraciones import *
-import psycopg2
 conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s port=%s"%(host,database,user,passwd,port))
 
 
@@ -103,30 +55,6 @@ cur.execute(sql)
 sql = """
 CREATE TABLE Animes_personajes
            (anime_id integer, personaje_id integer);
-"""
-
-cur.execute(sql)
-
-sql ="""
-CREATE TABLE Generos
-	(id serial PRIMARY KEY, nombre varchar(40));
-"""
-
-cur.execute(sql)
-
-
-conn.commit()
-cur.close()
-conn.close()
-
-           (id serial PRIMARY KEY, nombre varchar(140));
-"""
-
-cur.execute(sql)
-
-sql = """
-CREATE TABLE Animes_personajes
-           (anime_id integer, personajes_id integer);
 """
 
 cur.execute(sql)
